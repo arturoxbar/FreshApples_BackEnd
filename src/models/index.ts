3934cc0f4d21243c3e758ab54be154acf71b66c0
arturoxbar <101169728+arturoxbar@users.noreply.github.com>
2025-03-05 01:33:39 -0400

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,15 @@
 
 import User from "./User";
-
+import Media from "./Media";
+import Movie from "./Movie";
+import Serie from "./Serie";
+import MovieGenres from "./MovieGenres";
+import SerieGenres from "./SerieGenres";
 export {
-
+    Media,
+    Movie,
+    Serie,
     User,
+    MovieGenres,
+    SerieGenres
 }
