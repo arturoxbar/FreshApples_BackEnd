8af354e603ff89d7a3bae77fb8fefde92cfff6c8
arturoxbar <101169728+arturoxbar@users.noreply.github.com>
2025-03-06 21:03:39 -0400

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,37 +6,36 @@ export interface UserInterface {
     username: string;
     password: string;
     email: string;
-
+    code: string;
+    verified: boolean;
     verifyPassword: (password: string) => Promise<boolean>;
 }
 
-// Definición del esquema de usuario
+
 const UserSchema = new mongoose.Schema<UserInterface>({
     username: { type: String, required: true, unique: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, unique: false },
+    code: { type: String, required: false },
+    verified: { type: Boolean, require: true, default: false }
 
 });
 
-// Middleware para hashear la contraseña antes de guardar el usuario
 UserSchema.pre("save", async function (next) {
-    // Solo hashear la contraseña si ha sido modificada
     if (!this.isModified("password")) {
         return next();
     }
-    // Generar un salt y hashear la contraseña
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    // Continuar con el proceso de guardado
     next();
 });
 
-// Método para verificar la contraseña
+
 UserSchema.methods.verifyPassword = async function (password: string): Promise<boolean> {
     return await bcrypt.compare(password, this.password);
 };
 
-// Creación del modelo de usuario
+
 const User = mongoose.model<UserInterface>("User", UserSchema);
 
 export default User;
