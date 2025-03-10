cb09ed4b6e818243ab0761f7ef4844c3f621df97
arturoxbar <101169728+arturoxbar@users.noreply.github.com>
2025-03-09 22:31:55 -0400

    Update movieRoutes.ts

--- src/routes/movieRoutes.ts
+++ src/routes/movieRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { searchMedia, getMovie, getSerie, getOwnMedias } from "../controllers/movieControllers";
+import { searchMedia, getMovie, getSerie, getOwnMedias, getPopularMedias, getTrendingMedias } from "../controllers/movieControllers";
 import authenticate from "../middlewares/auth";
 
 const router = Router();
@@ -9,5 +9,7 @@ router.route("/general/:mediaTitle").post(searchMedia);
 router.get("/general", getOwnMedias)
 router.route("/movie/:id").get(getMovie);
 router.route("/serie/:id").get(getSerie);
+router.get("/general/popular", getPopularMedias);
+router.get("/general/trending", getTrendingMedias);
 
 export default router;
